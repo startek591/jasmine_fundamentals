737e98fd5af4e65d4de241f0e20dca8d1f8631df
jonathan_fambro <startek591@gmail.com>
2025-04-03 15:02:04 -0400

    The this keyword

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -45,4 +45,19 @@ describe('A suite with some shared setup', function () {
   it('should do something', () => {
     expect(true).toBe(true);
   });
+  describe('A spec', function () {
+    beforeEach(function () {
+      this.foo = 0;
+    });
+
+    it('can ise the `this` to share state', function () {
+      expect(this.foo).toEqual(0);
+      this.bar = 'test pollution';
+    });
+
+    it('prevent test pollution by having an empty `this` created for the next spec', function () {
+      expect(this.foo).toEqual(0);
+      expect(this.bar).toBe(undefined);
+    });
+  });
 });
