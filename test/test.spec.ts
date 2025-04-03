e09f9bf45f5287b792831e8e308abd53491415c7
jonathan_fambro <startek591@gmail.com>
2025-04-03 14:40:05 -0400

    Expectations - Matchers

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -12,3 +12,13 @@ describe('A suite is just a function', function () {
     expect(a).toBe(true);
   });
 });
+
+describe("The 'toBe' matcher compares with ===", function () {
+  it('and has a positive case', function () {
+    expect(true).toBe(true);
+  });
+
+  it('and can have a negative case', function () {
+    expect(false).not.toBe(true);
+  });
+});
