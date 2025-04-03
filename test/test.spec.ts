efd48f7ef15fda6fd76a958ef3942251728bbb82
jonathan_fambro <startek591@gmail.com>
2025-04-03 16:44:36 -0400

    Nesting describe Blocks

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -75,4 +75,33 @@ describe('A suite with some shared setup', function () {
       });
     });
   });
+
+  describe('A spec 2', function () {
+    let foo;
+
+    beforeEach(function () {
+      foo = 0;
+      foo += 1;
+    });
+
+    afterEach(function () {
+      foo = 0;
+    });
+
+    it('is just a function, so it can contain any code', function () {
+      expect(foo).toEqual(1);
+      expect(true).toEqual(true);
+    });
+
+    describe('nested inside a second describe', function () {
+      let bar;
+      beforeEach(function () {
+        bar = 1;
+      });
+
+      it('can reference both scopes as needed', function () {
+        expect(foo).toEqual(bar);
+      });
+    });
+  });
 });
