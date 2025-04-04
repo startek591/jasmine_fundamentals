431640fc50eb8f5c8c8ad43dc1347ca82b41c7ef
jonathan_fambro <startek591@gmail.com>
2025-04-04 15:37:27 -0400

    Spies

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -184,3 +184,76 @@ describe('long asynchronous specs', function () {
     await somethingSlow();
   }, 1000);
 });
+
+describe('A spy', function () {
+  let foo;
+  let bar = null;
+
+  beforeEach(function () {
+    foo = {
+      setBar: function (value) {
+        bar = value;
+      },
+    };
+
+    spyOn(foo, 'setBar');
+
+    foo.setBar(123);
+    foo.setBar(456, 'another param');
+  });
+
+  it('tracks that the spy was called', function () {
+    expect(foo.setBar).toHaveBeenCalled();
+  });
+
+  it('tracks that the spay was called', function () {
+    expect(foo.setBar).toHaveBeenCalledTimes(2);
+  });
+
+  it('tracks all the arguments of its calls', function () {
+    expect(foo.setBar).toHaveBeenCalledWith(123);
+    expect(foo.setBar).toHaveBeenCalledWith(456, 'another param');
+  });
+
+  it('track if it was called at all', function () {
+    foo.setBar();
+    expect(foo.setBar.calls.any()).toEqual(true);
+  });
+});
+
+describe('A spy when created manually', function () {
+  let whatAmI;
+
+  beforeEach(function () {
+    whatAmI = jasmine.createSpy('whatAmI');
+    whatAmI('I', 'am', 'a', 'spy');
+  });
+
+  it('tracks that the spy was called', function () {
+    expect(whatAmI).toHaveBeenCalled();
+  });
+});
+
+describe('Multiple spies, when created manually', function () {
+  let tape;
+
+  beforeEach(function () {
+    tape = jasmine.createSpyObj('tape', [
+      'play',
+      'pause',
+      'stop',
+      'rewind',
+    ]);
+
+    tape.play();
+    tape.pause();
+    tape.rewind(0);
+  });
+
+  it('creates spies for each requested function', function () {
+    expect(tape.play).toBeDefined();
+    expect(tape.pause).toBeDefined();
+    expect(tape.stop).toBeDefined();
+    expect(tape.rewind).toBeDefined();
+  });
+});
